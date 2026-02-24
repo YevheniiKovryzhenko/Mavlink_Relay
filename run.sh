a0df37d6c55a7ac1fec2e9b69244364d0e78772a
Yevhenii Kovryzhenko <yzk0058@auburn.edu>
2026-02-24 14:21:48 -0600

    1

--- run.sh
+++ run.sh
@@ -10,4 +10,6 @@ cmake -S . -B $BUILD_DIR
 cd $BUILD_DIR
 make
 
+# sudo ./Mavlink_Relay  -m 172.16.45.1 -mZ -mI 1 -pm #test mocap only
+# sudo ./Mavlink_Relay -m 172.16.45.1 -mI 1 -mZ -td /dev/ttyUSB1 -tb 230400 -ru 192.168.1.2 -pt
 sudo ./Mavlink_Relay -m 192.168.1.1 -mI 14 -mY -td /dev/ttyUSB0 -tb 230400 -ru 192.168.1.2 -pt
